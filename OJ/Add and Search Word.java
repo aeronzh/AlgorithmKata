fa42b08a753ab8314b87505b274beb212de160ce
oceanwu <oceaniatt@hotmail.com>
2016-06-05 16:53:00 +1200

    Update Add and Search Word.java

--- OJ/Add and Search Word.java
+++ OJ/Add and Search Word.java
@@ -9,16 +9,15 @@ class TrieNode {
 
 public class WordDictionary {
     private TrieNode root;
-
+    
     public WordDictionary() {
         root = new TrieNode();
     }
-
+    
     // Adds a word into the data structure.
     public void addWord(String word) {
         // Write your code here
         TrieNode cur = root;
-
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             if (!cur.children.containsKey(c)) {
@@ -26,7 +25,6 @@ public class WordDictionary {
             }
             cur = cur.children.get(c);
         }
-
         cur.isWord = true;
     }
 
@@ -36,32 +34,28 @@ public class WordDictionary {
         // Write your code here
         return find(word, 0, root);
     }
-
+    
     private boolean find(String word, int index, TrieNode cur) {
         if (index == word.length()) {
-            if (cur.children.size() == 0) {
-                return true;
-            } else {
-                return false;
-            }
+            return false;
         }
-
-        char c = word.charAt(index);
-        if (cur.children.containsKey(c)) {
-            if (index == word.length() - 1 && cur.children.get(c).isWord) {
-                return true;
+        
+        if (cur.children.containsKey(word.charAt(index))) {
+            if (index == word.length() - 1) {
+                return cur.children.get(word.charAt(index)).isWord;
             }
-            return find(word, index + 1, cur.children.get(c));
-        } else if (c == '.') {
-            for (TrieNode child : cur.children.values()) {
-                if (index == word.length() - 1 && child.isWord) {
+            return find(word, index + 1, cur.children.get(word.charAt(index)));
+        } else if (word.charAt(index) == '.') {
+            for (char c : cur.children.keySet()) {
+                if (index == word.length() - 1 && cur.children.get(c).isWord) {
                     return true;
                 }
-                if (find(word, index + 1, child)) { // if any of the path returns true
+                if (find(word, index + 1, cur.children.get(c))) {
                     return true;
                 }
             }
         }
+        
         return false;
     }
 }
@@ -69,3 +63,4 @@ public class WordDictionary {
 // Your WordDictionary object will be instantiated and called as such:
 // WordDictionary wordDictionary = new WordDictionary();
 // wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
