ef40269f77d93f9fec057e669ab82e6c1192d8de
oceanwu <oceanwu@Oceans-MacBook-Air.local>
2016-07-25 00:03:29 +1200

    Updates

--- OJ/Pow(x, n).java
+++ OJ/Pow(x, n).java
@@ -5,6 +5,41 @@
 // Hide Similar Problems (M) Sqrt(x)
 
 public class Solution {
+    /**
+     * @param x the base number
+     * @param n the power number
+     * @return the result
+     */
+    public double myPow(double x, int n) {
+        // Write your code here
+        if (n == 0) {
+            return 1.0;
+        }
+        
+        if (n > 0) {
+            return helper(x, n);
+        } else {
+            return 1.0 / helper(x, -n);
+        }
+    }
+    
+    private double helper(double x, int n) {
+        
+        if (n == 1) {
+            return x;
+        }
+        
+        double result = helper(x, n / 2);
+        
+        if (n % 2 == 0) {
+            return result * result;
+        } else {
+            return result * result * x;
+        }
+    }
+}
+
+public class Solution {
     public double myPow(double x, int n) {
         if (n >= 0) {
             return pow(x, n);
